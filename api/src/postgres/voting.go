d09ea0bd69ac2822378738a9b1f8369f39159331
Lbarto12 <lbarto12@hotmail.com>
2024-09-28 04:22:12 -0400

    Unhandled SQLX hotfix. Ignored :)

--- api/src/postgres/voting.go
+++ api/src/postgres/voting.go
@@ -8,7 +8,7 @@ import (
 
 func hashFunc(s string) string {
 	h := fnv.New64a()
-	h.Write([]byte(s))
+	_, _ = h.Write([]byte(s))
 	return fmt.Sprintf("%x", h.Sum64())
 }
 
@@ -55,24 +55,24 @@ func Vote(voter VoterRequest, candidate string) error {
 
 	query, err := tx.NamedQuery(`UPDATE voter SET has_voted = true WHERE id = :id;`, voterData)
 	if err != nil {
-		tx.Rollback()
+		_ = tx.Rollback()
 		return err
 	}
 	err = query.Close()
 	if err != nil {
-		tx.Rollback()
+		_ = tx.Rollback()
 		return err
 	}
 
 	_, err = tx.Exec(`UPDATE polls SET votes = votes + 1 WHERE candidate = $1;`, candidateId)
 	if err != nil {
-		tx.Rollback()
+		_ = tx.Rollback()
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
-		tx.Rollback()
+		_ = tx.Rollback()
 		return err
 	}
 
