718aa021744de4f3ac4c790654a1d6e7f8dbb433
aali233 <aali23@ubishops.ca>
2024-10-01 21:54:01 -0400

    Updated +page.svelte

--- tabulation/src/routes/+page.svelte
+++ tabulation/src/routes/+page.svelte
@@ -1,3 +1,21 @@
 
 
-<button class="btn btn-primary">Hello World</button>
+
+
+   <script>
+    import BarChart from './BarChart.svelte';
+    import PieChart from './PieChart.svelte';
+    import HorizontalBarChart from './HorizontalBarChart.svelte';
+</script>
+
+<div>
+    <h2>Bar Chart</h2>
+    <BarChart />
+
+
+    <PieChart />
+
+    <h2>Horizontal Bar Chart</h2>
+    <HorizontalBarChart />
+</div>
+
