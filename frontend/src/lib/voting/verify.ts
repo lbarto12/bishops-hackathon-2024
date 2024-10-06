b8c5a6f2a0ff256ac4e8a6cea6d8a762b7fbaf3d
Lbarto12 <lbarto12@hotmail.com>
2024-10-06 18:23:37 -0400

    Implemented hashing sha256 on frontend for request

--- frontend/src/lib/voting/verify.ts
+++ frontend/src/lib/voting/verify.ts
@@ -1,16 +1,18 @@
 
 import {PUBLIC_API_HOST} from "$env/static/public";
+import {sha256} from "js-sha256";
 
 
 export async function vote(name: string, healthCard: string, candidate: string): Promise<Response> {
+
+    const hash = sha256.create();
+    hash.update(`${name}${healthCard}${candidate}`);
+    const final: string = hash.hex();
+
     return await fetch(PUBLIC_API_HOST + "/vote", {
         method: "POST",
         body: JSON.stringify({
-            voter: {
-                name: name,
-                health_card: healthCard,
-            },
-            candidate: candidate,
+            data: final
         })
     });
 }
